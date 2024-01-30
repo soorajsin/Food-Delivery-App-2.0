cdc72fe88937bf4c9338e6b1d297d0c07181ce12
Sooraj Singh <soorajsingh7505@gmail.com>
2024-01-30 19:20:15 +0530

    upload

---
+++ client/src/Components/Navbar/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AppBar, Toolbar } from "@mui/material";
+
+const Nav = () => {
+  return (
+    <>
+      <AppBar>
+        <Toolbar></Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Nav;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 require("./DB/Connection");
+const cors = require("cors");
+const router = require("./Routes/Route");
+const cookieParser = require("cookie-parser");
 const port = 4000 || process.env.PORT;
 
 app.get("/", (req, res) => {
@@ -8,6 +11,11 @@ app.get("/", (req, res) => {
     msg: "Server Start"
   });
 });
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`Server start on port ${port}`);
